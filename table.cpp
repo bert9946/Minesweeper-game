ae629f02068c1f6b4e2d47b136328cbedc7c9f3a
bert9946 <bert9946@gmail.com>
2021-06-01 16:25:29 +0800

    Minor changes.

--- table.cpp
+++ table.cpp
@@ -154,7 +154,7 @@ void Table::printTable() const
 		cout << endl;
 	}
 	cout << endl;
-	cout << "$: " << this->pArray[1][1].getNumberOfFlag() << '/' << this->numberOfMines << endl;
+	cout << "$: " << Block::getNumberOfFlag() << '/' << this->numberOfMines << endl;
 }
 
 //Whether the coordinates is valid in the table.
